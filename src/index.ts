cf89a4fbd4718e8702d905ee3d7911c8822449af
Himanshu Jangid <iamahalofan@gmail.com>
2023-05-11 23:51:26 +0530

    🚧 thinking of input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
+import { timeUtil } from "./utils";
 
+// days to use this week
+const days = [1, 2, 3, 4, 5, 6];
+
+const classes = [1, 2, 3, 4, 5];
+
+const classDuration = {
+  1: 60,
+  2: 60,
+  3: 60,
+  4: 60,
+  5: 120,
+};
+
+const classFreqPerWeek = {
+  1: 2,
+  2: 4,
+  3: 3,
+  4: 2,
+  5: 1,
+};
+
+const slots = 24 * 15;
+
+const dayTimes = {
+  startTime: "09:00",
+  endTime: "04:00",
+};
