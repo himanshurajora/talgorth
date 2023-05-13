5c874afe09946b36c48888180e029b7abce7a86e
Himanshu Jangid <iamahalofan@gmail.com>
2023-05-14 04:31:17 +0530

    gap implementation

--- src/single-subject-test.ts
+++ src/single-subject-test.ts
@@ -4,18 +4,23 @@ const total = 5;
 let used = 0;
 let slots = 6;
 
+let minGap = 8;
+
 const table: string[][] = [];
+let gap = 7;
 for (let i = 0; i < days; i++) {
   const day: string[] = [];
   let probability = 0;
   for (let j = 0; j < slots; j++) {
     probability = (total - used) / (days - i);
     console.log(probability);
-    if (probability > 0.9) {
+    if (probability > 0.9 && gap > minGap) {
+      gap = 0;
       used++;
       day.push("YES");
       continue;
     }
+    gap++;
     day.push("NO");
   }
 
