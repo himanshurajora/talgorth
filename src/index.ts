8f9fd44386ac23aab2f2a31f3a9f022edc1d8c80
Himanshu Jangid <iamahalofan@gmail.com>
2023-05-17 23:06:50 +0530

    multi subject basic no gap

--- src/index.ts
+++ src/index.ts
@@ -3,88 +3,73 @@ import * as _ from "lodash";
 // days to use this week
 const days = [1, 2, 3, 4, 5, 6];
 
-// minutes in hour
-const minutesInHour = 60;
+const slotsPerDay = 6;
 
-const classes = [1];
-
-// duration in minutes
-const classDuration = {
-  1: 60,
-};
-
-const minimumSlot = _.reduce(
-  classDuration,
-  (prev, curr) => {
-    if (curr < prev) {
-      return curr;
-    }
-
-    return prev;
-  },
-  Infinity
-);
-
-const classSlotSize = _.mapValues(
-  classDuration,
-  (duration) => duration / minimumSlot
-);
-
-const classFreqPerWeek = {
-  1: 5,
-} as any;
-
-const classUsedFreq = _.mapValues(classFreqPerWeek, () => 0) as any;
-
-const dayTimes = {
-  startTime: "09:00",
-  endTime: "15:00",
-};
-
-const startTimeMoment = moment(dayTimes.startTime, "HH:mm");
-const endTimeMoment = moment(dayTimes.endTime, "HH:mm");
-
-const totalHourPerDay = Math.abs(startTimeMoment.diff(endTimeMoment, "hours"));
-const totalDays = days.length;
-const totalSlots = totalHourPerDay / (minimumSlot / 60);
-const classPerDay = _.mapValues(classFreqPerWeek, (freq) => freq / totalDays);
-
-console.log({ totalHourPerDay });
-console.log({ totalSlots });
-console.log({ totalDays });
-console.log({ classPerDay });
-console.log({ classSlotSize });
-console.log({ minimumSlot });
-
-const timetable = [];
-
-for (let day of days) {
-  const dayTable = [];
-  for (
-    let start = startTimeMoment.clone(), end = endTimeMoment.clone();
-    start.isBefore(end);
-    start.add(60, "minutes")
-  ) {
-    let nearest = -1;
-    for (let c in classFreqPerWeek) {
-      console.log(
-        "CALC",
-        (classFreqPerWeek[c] - classUsedFreq[c]) / classFreqPerWeek[c]
-      );
-      if (classFreqPerWeek[c] - classUsedFreq[c] > 0) {
-        nearest = +c;
-      }
-    }
-
-    dayTable.push(nearest);
-
-    if (nearest === -1) {
-      dayTable.push();
-    } else {
-      classUsedFreq[nearest] += 1;
-    }
-  }
-  timetable.push(dayTable);
+enum Subjects {
+  MATH = "MATH",
+  SCIENCE = "SCIENCE",
+  EMPTY = "EMPTY",
 }
 
-console.log(timetable);
+const classData = [
+  {
+    name: Subjects.MATH,
+    freq: 5,
+    used: 0,
+    size: 1,
+  },
+  {
+    name: Subjects.SCIENCE,
+    freq: 6,
+    used: 0,
+    size: 1,
+  },
+];
+
+type ClassData = (typeof classData)[0];
+
+const classSlotSize = {
+  [Subjects.MATH]: 1,
+  [Subjects.SCIENCE]: 1,
+};
+
+const tt: string[][] = [];
+for (let i = 0; i < days.length; i++) {
+  const td: string[] = [];
+  for (let j = 0; j < slotsPerDay; j++) {
+    const toPut = getMinimumDistanceClass(classData, i);
+    if (toPut) {
+      td.push(toPut.name);
+      const index = _.findIndex(classData, (c) => c.name === toPut.name);
+      classData[index].used += 1;
+      continue;
+    }
+
+    td.push(Subjects.EMPTY);
+  }
+  tt.push(td);
+}
+
+console.table(tt);
+
+function getMinimumDistanceClass(input: ClassData[], day: number) {
+  const classWithProbabilities = _.map(input, (c) => {
+    return {
+      ...c,
+      probability: (c.freq - c.used) / (days.length - day),
+    };
+  });
+
+  const sorted = classWithProbabilities.sort(
+    (a, b) => b.probability - a.probability
+  );
+  // const sorted = _.sortBy(classWithProbabilities, 'probability')
+  const top = _.head(sorted);
+
+  if (!top || top?.probability < 0.9) return null;
+
+  return top;
+}
+
+const res = getMinimumDistanceClass(classData, 0);
+res;
---
+++ src/old/test1.ts
@@ -0,0 +1,88 @@
+// Was just a basic trial, I think it failed
+
+import moment from "moment";
+import * as _ from "lodash";
+// days to use this week
+const days = [1, 2, 3, 4, 5, 6];
+
+// minutes in hour
+const minutesInHour = 60;
+
+const classes = [1];
+
+// duration in minutes
+const classDuration = {
+  1: 60,
+};
+
+const minimumSlot = _.reduce(
+  classDuration,
+  (prev, curr) => {
+    if (curr < prev) {
+      return curr;
+    }
+
+    return prev;
+  },
+  Infinity
+);
+
+const classSlotSize = _.mapValues(
+  classDuration,
+  (duration) => duration / minimumSlot
+);
+
+const classFreqPerWeek = {
+  1: 5,
+} as any;
+
+const classUsedFreq = _.mapValues(classFreqPerWeek, () => 0) as any;
+
+const dayTimes = {
+  startTime: "09:00",
+  endTime: "15:00",
+};
+
+const startTimeMoment = moment(dayTimes.startTime, "HH:mm");
+const endTimeMoment = moment(dayTimes.endTime, "HH:mm");
+
+const totalHourPerDay = Math.abs(startTimeMoment.diff(endTimeMoment, "hours"));
+const totalDays = days.length;
+const totalSlots = totalHourPerDay / (minimumSlot / 60);
+const classPerDay = _.mapValues(classFreqPerWeek, (freq) => freq / totalDays);
+
+console.log({ totalHourPerDay });
+console.log({ totalSlots });
+console.log({ totalDays });
+console.log({ classPerDay });
+console.log({ classSlotSize });
+console.log({ minimumSlot });
+
+const timetable = [];
+
+for (let i in days) {
+  const dayTable = [];
+  for (
+    let start = startTimeMoment.clone(), end = endTimeMoment.clone();
+    start.isBefore(end);
+    start.add(60, "minutes")
+  ) {
+    let nearest = -1;
+    for (let c in classFreqPerWeek) {
+      if (classFreqPerWeek[c] - classUsedFreq[c] > 0) {
+        nearest = +c;
+      }
+    }
+
+    dayTable.push(nearest);
+
+    if (nearest === -1) {
+      dayTable.push();
+    } else {
+      classUsedFreq[nearest] += 1;
+    }
+  }
+  timetable.push(dayTable);
+}
+
+console.log(timetable);
